ddafa84a20b47e46a07fa5b9ce2433735dae18a7
Sezer Iltekin <sezer@iltekin.com>
2022-10-30 23:21:45 +0300

    Add new app: CardWorkout-Programmatic

---
+++ UIKit/CardWorkout-Programmatic/CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,20 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Sezer İltekin on 19.10.2022.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
