d6c089789ae739df071ffea87be10acf16ab4403
Sezer Iltekin <sezer@iltekin.com>
2023-01-05 01:44:13 +0300

    updated app

--- UIKit/Clima-iOS13/Clima/Model/Environment.swift
+++ UIKit/Clima-iOS13/Clima/Model/Environment.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+public struct Environment {
+    enum Keys {
+        static let apiKey = "API_KEY"
+    }
+    
+    // Get the BASE_URL
+    static let apiKey: String = {
+        guard let apiKeyProperty = Bundle.main.object(
+            forInfoDictionaryKey: Keys.apiKey
+        ) as? String else {
+            fatalError("API_KEY not found")
+        }
+        return apiKeyProperty
+    }()
+}
